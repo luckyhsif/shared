495d6337b584b56b84cc43f7f56eaca39b709a5b
David Sag <davesag@gmail.com>
2013-09-03 12:30:14 +1000

    puzzling out how to validate an Iteration's entry count and balance

--- interaction.rb
+++ interaction.rb
@@ -2,22 +2,10 @@ class Interaction < ActiveRecord::Base
   has_many :entries, class_name: 'LedgerEntry'
   validates_presence_of :note
   
-  validate :entries_must_sum_to_zero
-  
   def balance
     credit = self.entries.sum(:credit)
     debit = self.entries.sum(:debit)
     return credit - debit
   end
   
-  protected
-  
-  def entries_must_sum_to_zero
-    if self.entries.count.odd?
-      errors.add(:entries, "There must be an even number of entries.")
-    end
-    if self.balance != 0
-      errors.add(:entries, "Entries must sum to zero.")
-    end
-  end
 end
