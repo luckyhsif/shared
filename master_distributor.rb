da6fb94f70542c0932cdef06189c059e92b45303
Daphne Chan <yiingshan@gmail.com>
2013-09-04 14:49:58 +1000

    validation for distributor may not share same location

--- master_distributor.rb
+++ master_distributor.rb
@@ -1,10 +1,17 @@
 class MasterDistributor < User
   has_many :locations, inverse_of: :master_distributor
-  validate :has_a_location
+  validate :has_a_location, :may_not_have_same_location
   protected
   def has_a_location
     if self.locations.empty?
       errors.add(:location, "MasterDistributor must have at least one Location.")
     end
+  end
+
+  def may_not_have_same_location
+  	md_id = self.locations.last 
+  	  if md_id.master_distributor_id != self.id 
+  	  	errors.add(:location, "Shares the same location as another MasterDistributor")
+  	  end
   end
 end
