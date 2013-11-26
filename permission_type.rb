ed4a91522111228358ebe701a79b5f12bbea6d13
Francois van der Hoven <francois777@optusnet.com.au>
2013-11-27 09:20:58 +1000

    Implement database changes on permission tables and associations

--- permission_type.rb
+++ permission_type.rb
@@ -1,5 +1,5 @@
 class PermissionType < ActiveRecord::Base
-  has_many :permissions
+  has_many :permissions, foreign_key: :permission_type_id
   validates :name,  :presence   => true, 
                     :length     => { :minimum => 5, :maximum => 50 }, 
                     :uniqueness => { :case_sensitive => false }
