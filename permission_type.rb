f722c7b1e5e63384a25ea872eb89e8c90029a081
Francois van der Hoven <francois777@optusnet.com.au>
2013-11-26 21:20:27 +1000

    Refactoring

--- permission_type.rb
+++ permission_type.rb
@@ -1,9 +1,7 @@
 class PermissionType < ActiveRecord::Base
-  belongs_to :granting_role_type, class_name: 'Role'
-  belongs_to :receiving_role_type, class_name: 'Role' 
-  #   , join_table: 'grantable_permissions_roles', association_foreign_key: :role_id
-
+  has_many :permissions
   validates :name,  :presence   => true, 
                     :length     => { :minimum => 5, :maximum => 50 }, 
                     :uniqueness => { :case_sensitive => false }
+
 end
