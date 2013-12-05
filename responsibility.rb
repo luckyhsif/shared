455edf282b519191cf0507006fa0e0cfaa5163b5
Francois van der Hoven <francois777@optusnet.com.au>
2013-12-05 20:38:32 +1000

    Corrected password reset function and worked on display templates

--- responsibility.rb
+++ responsibility.rb
@@ -3,4 +3,9 @@ class Responsibility < ActiveRecord::Base
   belongs_to :role
   belongs_to :location
 
+  def self.find_responsibility_for_location(userid, locid)
+    resps = Responsibility.where("user_id=? AND location_id=?", userid, locid)
+    return nil if resps.empty?
+    resps.first
+  end
 end
